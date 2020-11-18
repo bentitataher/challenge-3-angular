8a374fa845bc11711c8664a5fade9723b5aa92f1
bentitataher <bentitataher@gmail.com>
2020-11-18 14:28:54 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'challenge-III';
-}
+  topics = ["Angular", "React", "Vue"];
+
+  userModel = new User('','rob@test.com',5556665566,'','morning',true);
+  
+
+
+} // Fin - export class AppComponent
